37800488c16e9590a26ccf11fb3c8ec002b7480c
Max Wittal <wittal@neobotix.de>
2020-06-17 15:20:35 +0200

    codegen update

--- codegen.sh
+++ codegen.sh
@@ -2,7 +2,13 @@
 
 VNX_INTERFACE_DIR=${VNX_INTERFACE_DIR:-/usr/interface}
 
+BASE_INCLUDE=$1
+
+if [ -z "$BASE_INCLUDE" ]; then
+	BASE_INCLUDE=${VNX_INTERFACE_DIR}/pilot/base/
+fi
+
 cd $(dirname "$0")
 
-vnxcppcodegen --cleanup generated/ pilot.usboard interface/ modules/ ${VNX_INTERFACE_DIR}/vnx/ ${VNX_INTERFACE_DIR}/pilot/base/
+vnxcppcodegen --cleanup generated/ pilot.usboard interface/ modules/ ${VNX_INTERFACE_DIR}/vnx/ ${BASE_INCLUDE}
 
