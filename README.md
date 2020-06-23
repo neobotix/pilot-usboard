05509cfc0cd2c21c52dca8a59f7f89cc4e71fcc1
Max Wittal <wittal@neobotix.de>
2020-06-23 18:00:24 +0200

    codegen update

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ Provides the USBoard module as an interface to the board via Serial or CAN bus.
 
 ## Dependencies
 
-`pilot-base`
+`pilot-base vnx-base`
--- codegen.sh
+++ codegen.sh
@@ -3,12 +3,17 @@
 VNX_INTERFACE_DIR=${VNX_INTERFACE_DIR:-/usr/interface}
 
 BASE_INCLUDE=$1
+VNX_INCLUDE=$2
 
 if [ -z "$BASE_INCLUDE" ]; then
 	BASE_INCLUDE=${VNX_INTERFACE_DIR}/pilot/base/
 fi
 
+if [ -z "$VNX_INCLUDE" ]; then
+	VNX_INCLUDE=${VNX_INTERFACE_DIR}/vnx/
+fi
+
 cd $(dirname "$0")
 
-vnxcppcodegen --cleanup generated/ pilot.usboard interface/ modules/ ${VNX_INTERFACE_DIR}/vnx/ ${BASE_INCLUDE}
+vnxcppcodegen --cleanup generated/ pilot.usboard interface/ modules/ $BASE_INCLUDE $VNX_INCLUDE
 
